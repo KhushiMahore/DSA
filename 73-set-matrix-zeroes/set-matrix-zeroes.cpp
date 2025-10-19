10e150a12b8a193fb843f220e32c6081b0621282
Khushi Mahore <its.khushi.mahore@gmail.com>
2025-10-19 23:45:11 +0530

    Time: 1 ms (15.44%) | Memory: 18.5 MB (95.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> row(matrix.size(), 1);
-        vector<int> col(matrix[0].size(), 1);
+
+        vector<int> row(matrix.size(), 1);    //{1,0,1}
+        vector<int> col(matrix[0].size(), 1); //{1,0,1}
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[0].size(); j++) {
                 if (matrix[i][j] == 0) {
@@ -11,15 +12,15 @@ public:
                 }
             }
         }
+
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[0].size(); j++) {
                 if (row[i] == 0 || col[j] == 0) {
                     matrix[i][j] = 0;
                 }
-                if (col[j] == 0) {
-                    matrix[i][j] = 0;
-                }
             }
         }
     }
-};
+
+}
+;
