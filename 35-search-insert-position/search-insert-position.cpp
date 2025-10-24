067e035f47e3e25b78d43cc9e46c3354b0635da8
Khushi Mahore <its.khushi.mahore@gmail.com>
2025-10-24 21:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.79%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,20 +4,19 @@ public:
 
         int low = 0;
         int high = nums.size() - 1;
+        int ans = nums.size();
 
         while (low <= high) {
-            int mid =( low + high) / 2;
+            int mid = (low + high) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+            if (nums[mid] >= target) {
+                ans = mid;
                 high = mid - 1;
+            } else {
+
+                low = mid + 1;
             }
         }
-    
-    return low;
-}
-}
-;
+        return ans;
+    }
+};
